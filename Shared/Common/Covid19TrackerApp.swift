8dda0b425c8275a9acea5240287028f828c1b2be
trnserdar <trnserdar@gmail.com>
2020-07-07 22:20:45 +0300

    added statistic view.

--- Shared/Common/Covid19TrackerApp.swift
+++ Shared/Common/Covid19TrackerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Covid19TrackerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StatisticView()
         }
     }
 }
---
+++ Shared/Views/StatisticView.swift
@@ -0,0 +1,20 @@
+//
+//  StatisticView.swift
+//  Covid19Tracker
+//
+//  Created by Serdar Turan on 7.07.2020.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatisticView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticView()
+    }
+}
