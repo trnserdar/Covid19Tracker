4b97b66da2222d3790a19d25c7228ad353b643bd
trnserdar <trnserdar@gmail.com>
2020-07-07 22:11:16 +0300

    Initial Commit

---
+++ Shared/Covid19TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19TrackerApp.swift
+//  Shared
+//
+//  Created by Serdar Turan on 7.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct Covid19TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
