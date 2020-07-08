3a9dd5036880ff703057162938b5c4661ca2273c
trnserdar <trnserdar@gmail.com>
2020-07-08 22:11:03 +0300

    added daily report widget.

---
+++ DailyReport/DailyReport.swift
@@ -0,0 +1,70 @@
+//
+//  DailyReport.swift
+//  DailyReport
+//
+//  Created by Serdar Turan on 8.07.2020.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    public typealias Entry = SimpleEntry
+
+    public func snapshot(with context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    public func timeline(with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    public let date: Date
+}
+
+struct PlaceholderView : View {
+    var body: some View {
+        Text("Placeholder View")
+    }
+}
+
+struct DailyReportEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct DailyReport: Widget {
+    private let kind: String = "DailyReport"
+
+    public var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider(), placeholder: PlaceholderView()) { entry in
+            DailyReportEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct DailyReport_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
