1c83945d75c5af2fd0a06af33642cbf53e711bfc
trnserdar <trnserdar@gmail.com>
2020-07-11 11:54:50 +0300

    added test data in statistic model.

--- Shared/Models/StatisticModel.swift
+++ Shared/Models/StatisticModel.swift
@@ -17,4 +17,6 @@ struct StatisticModel: Codable {
     let tests: NumbersModel?
     let day: String?
     let time: String?
+    
+    static let testData = StatisticModel(continent: Continent.asia, country: "Turkey", population: 84349028, cases: NumbersModel(new: "+1086", active: 18608, critical: 1130, recovered: 182995, the1MPop: nil, total: 206844), deaths: NumbersModel(new: "+16", active: nil, critical: nil, recovered: nil, the1MPop: nil, total: 5241), tests: NumbersModel(new: nil, active: nil, critical: nil, recovered: nil, the1MPop: nil, total: 3682673), day: "2020-07-07", time: "2020-07-07T12:30:06+00:00")
 }
